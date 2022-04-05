b3b9bcdb3be88d18ef5c705b69c4640bc7977003
Flavio Vieira <flavio.vl@gmail.com>
2022-04-04 22:02:57 -0300

    chore: add function to merge adjacent registers

--- map_csv.py
+++ map_csv.py
@@ -49,26 +49,71 @@ def buid_group_map(map_block, group: str) -> List[OrderedDict[str, Union[str, in
     return group_map
 
 
+def build_range_map(
+    registers: List[Tuple[int, int]], max_reg_request: int = 100
+) -> List[Tuple[int, int]]:
+
+    register_ranges: List[Tuple[int, int]] = []
+    register_start: int = registers[0][0]
+    size_request: int = registers[0][1]
+    current: int = registers[0][0]
+    register_counter: int = 0
+
+    for addr, size in registers[1:]:
+        if addr == current + size and register_counter < max_reg_request:
+            size_request += size
+            register_counter += 1
+            # continue
+
+        else:
+            register_ranges.append((register_start, size_request))
+            register_start = addr
+            size_request = size
+            register_counter = 0
+
+        current = addr
+
+    register_ranges.append((register_start, size_request))
+    return register_ranges
+
+
 def main():
 
     path_file = "maps/full_md30_true.csv"
     # path_file = "maps/med_md30.csv"
     # localhost =
 
+    raw_map_block: List[OrderedDict[str, str]] = []
+    
+    register_minutely: List[OrderedDict[str, Union[str, int]]] = []
+    register_quartely: List[OrderedDict[str, Union[str, int]]] = []
+    register_monthly: List[OrderedDict[str, Union[str, int]]] = []
+
+    address_minutely: List[Tuple[int, int]] = []
+    address_quartely: List[Tuple[int, int]] = []
+    address_monthly: List[Tuple[int, int]] = []
+
     template = ["register", "address", "size", "type", "group"]
     group_minutely: str = "minutely"
     group_quartely: str = "quartely"
     group_monthly: str = "monthly"
 
-    raw_map_block: List[OrderedDict[str, str]] = []
+    max_reg_request: int = 100
+
     raw_map_block = csv_map_parser(path_file)
-    
     valid_map_block = modbus_decoder_map(raw_map_block, template)
 
     register_minutely = buid_group_map(valid_map_block, group_minutely)
     register_quartely = buid_group_map(valid_map_block, group_quartely)
     register_monthly = buid_group_map(valid_map_block, group_monthly)
     
+    address_minutely = [(line["address"], line["size"]) for line in register_minutely]
+    address_quartely = [(line["address"], line["size"]) for line in register_quartely]
+    address_monthly = [(line["address"], line["size"]) for line in register_monthly]
+    
+    group_request_minutely = build_range_map(address_minutely, max_reg_request)
+    group_request_quartely = build_range_map(address_quartely, max_reg_request)
+    group_request_monthly = build_range_map(address_monthly, max_reg_request)
 # ====================================================================================================
 
 
