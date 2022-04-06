ed9417914885d7bc19e375c95f9d700a2a5fb39a
Flavio Vieira <flavio.vl@gmail.com>
2022-04-06 13:03:57 -0300

    refactor: logic execution functions and return function main

--- map_csv.py
+++ map_csv.py
@@ -1,7 +1,9 @@
 from collections import OrderedDict
 from csv import DictReader
-from typing import Dict, List, NewType, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
+from config import devices_config
+from constants import register_map_columns
 from utils import str_bool
 
 
@@ -19,9 +21,7 @@ def csv_map_parser(file_path) -> List[OrderedDict[str, str]]:
     return raw_map_block
 
 
-def modbus_decoder_map(
-    raw_map_block, template
-) -> List[OrderedDict[str, Union[str, int]]]:
+def modbus_decoder_map(raw_map_block, template) -> List[OrderedDict[str, Union[str, int]]]:
     valid_map_block: List[OrderedDict[str, Union[str, int]]] = []
 
     for line in raw_map_block:
@@ -35,7 +35,6 @@ def modbus_decoder_map(
                 if column in template:
                     valid_line[column] = line[column]
             valid_map_block.append(valid_line)
-
     return valid_map_block
 
 
@@ -50,9 +49,7 @@ def buid_group_map(map_block, group: str) -> List[OrderedDict[str, Union[str, in
     return group_map
 
 
-def build_range_map(
-    registers: List[Tuple[int, int]], max_reg_request: int = 100
-) -> List[Tuple[int, int]]:
+def build_range_map(registers: List[Tuple[int, int]], max_reg_request=100) -> List[Tuple[int, int]]:
 
     register_ranges: List[Tuple[int, int]] = []
     register_start: int = registers[0][0]
@@ -78,58 +75,37 @@ def build_range_map(
     return register_ranges
 
 
-def print_registers(text, regs):
-    print("---" * 30)
-    print(text)
-    print("")
-    print("Tuple(addr_initial, size):", regs)
+def group_reg_request(valid_map_block, group, max_reg_request):
+    map_group_full = buid_group_map(valid_map_block, group)
+    map_group_request = [(line["address"], line["size"]) for line in map_group_full]
+    return build_range_map(map_group_request, max_reg_request)
 
 
-def main():
+def run():
+    path_file = devices_config["MD30"]["path_file"]
+    max_reg_request: int = devices_config["MD30"]["max_reg_request"]
 
-    path_file = "maps/full_md30_true.csv"
-    # path_file = "maps/med_md30.csv"
-    # localhost =
-
-    raw_map_block: List[OrderedDict[str, str]] = []
-
-    register_minutely: List[OrderedDict[str, Union[str, int]]] = []
-    register_quartely: List[OrderedDict[str, Union[str, int]]] = []
-    register_monthly: List[OrderedDict[str, Union[str, int]]] = []
-
-    address_minutely: List[Tuple[int, int]] = []
-    address_quartely: List[Tuple[int, int]] = []
-    address_monthly: List[Tuple[int, int]] = []
-
-    template = ["register", "address", "size", "type", "group"]
     group_minutely: str = "minutely"
     group_quartely: str = "quartely"
     group_monthly: str = "monthly"
 
-    max_reg_request: int = 100
+    raw_map_block: List[OrderedDict[str, str]] = csv_map_parser(path_file)
+    valid_map_block: List[OrderedDict[str, str | int]] = modbus_decoder_map(raw_map_block, register_map_columns)
 
-    raw_map_block = csv_map_parser(path_file)
-    valid_map_block = modbus_decoder_map(raw_map_block, template)
+    group_request_minutely = group_reg_request(valid_map_block, group_minutely, max_reg_request)
+    group_request_quartely = group_reg_request(valid_map_block, group_quartely, max_reg_request)
+    group_request_monthly = group_reg_request(valid_map_block, group_monthly, max_reg_request)
 
-    register_minutely = buid_group_map(valid_map_block, group_minutely)
-    register_quartely = buid_group_map(valid_map_block, group_quartely)
-    register_monthly = buid_group_map(valid_map_block, group_monthly)
-
-    address_minutely = [(line["address"], line["size"]) for line in register_minutely]  # type: ignore
-    address_quartely = [(line["address"], line["size"]) for line in register_quartely]  # type: ignore
-    address_monthly = [(line["address"], line["size"]) for line in register_monthly]    # type: ignore
-
-    group_request_minutely = build_range_map(address_minutely, max_reg_request)
-    group_request_quartely = build_range_map(address_quartely, max_reg_request)
-    group_request_monthly = build_range_map(address_monthly, max_reg_request)
-
-# ==================================================================================================
-    print_registers("Group de Request Minutely:", group_request_minutely)
-    print_registers("Group de Request Quartely:", group_request_quartely)
-    print_registers("Group de Request Monthly:", group_request_monthly)
-    print("---" * 30)
-# ==================================================================================================
+    registers: Dict[str, List[Tuple[int, int]]] = {
+        "Minutely": group_request_minutely,
+        "Quartely": group_request_quartely,
+        "Monthly": group_request_monthly,
+    }
+    return registers
+    # return {"Minutely": group_request_minutely, "Quartely": group_request_quartely, "Monthly": group_request_monthly,}
+    
+# ======================================================================================================================
 
 
 if __name__ == "__main__":
-    main()
+    run()
