06036541b8b379958a090639d38906d2b9196b9e
Flavio Vieira <flavio.vl@gmail.com>
2022-04-06 10:10:19 -0300

    chore: add devices config

---
+++ config.py
@@ -0,0 +1,30 @@
+devices_config = {
+    'MD30': {
+        "serial_number": "010101",
+        "model": "MD30",
+        "ip_address": "164.41.20.228",
+        "byteorder": "Endian.Big",
+        "wordorder": "Endian.Little",
+        "port": 1001,
+        "protocol": 0,               # tcp
+        "max_reg_request": 100,
+        "path_file": "maps/full_md30_true.csv",
+    },
+    
+    'TR4020': {
+        "serial_number": "020202",
+        "model": "TR4020",
+        "ip_address": "172.16.3.100",
+        "port": 1001,
+        "protocol": "tcp",
+        "path_file": "maps/full_md30_true.csv"
+    },
+    'Konect': {
+        "serial_number": "030303",
+        "model": "Konect",
+        "ip_address": "172.16.3.150",
+        "port": 1001,
+        "protocol": "tcp",
+        "path_file": "maps/full_md30_true.csv"
+    },
+}
